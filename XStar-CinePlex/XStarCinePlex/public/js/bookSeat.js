327d92621aaf7bf6c62a9a96be01fc99027c8284
CodeLTX <letuanxuyen92@gmail.com>
2021-06-18 19:04:52 +0700

    change BookSeat.plade

--- XStar-CinePlex/XStarCinePlex/public/js/bookSeat.js
+++ XStar-CinePlex/XStarCinePlex/public/js/bookSeat.js
@@ -1,4 +1,4 @@
-const container = document.getElementById('containerStage');
+const containerSeat = document.querySelector('.chooseSeat');
 const seats = document.querySelectorAll('.row .seat:not(.occupied');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
@@ -7,36 +7,9 @@ const seatSelect = document.querySelectorAll('.rowSeat');
 populateUI();
 let chooseSeat = seatSelect.value;
 
-// Save selected movie index and price
-function setMovieData(movieIndex, moviePrice) {
-  localStorage.setItem('selectedMovieIndex', movieIndex);
-  localStorage.setItem('selectedMoviePrice', moviePrice);
-}
-
-// update total and count
-function updateSelectedCount() {
-  const selectedSeats = document.querySelectorAll('.row .seat.selected');
-
-  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
-
-  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
-
-  //copy selected seats into arr
-  // map through array
-  //return new array of indexes
-
-  const selectedSeatsCount = selectedSeats.length;
-
-  count.innerText = selectedSeatsCount;
-
-  // total.innerText = selectedSeatsCount * ticketPrice;
-
-
-}
-
 // get data from localstorage and populate ui
 function populateUI() {
-  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
   if (selectedSeats !== null && selectedSeats.length > 0) {
     seats.forEach((seat, index) => {
       if (selectedSeats.indexOf(index) > -1) {
@@ -44,33 +17,18 @@ function populateUI() {
       }
     });
   }
-
-  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
-
-  if (selectedMovieIndex !== null) {
-    selectedIndex = selectedMovieIndex;
-  }
 }
-
-// Movie select event
-container.addEventListener('change', (e) => {
-  chooseSeat = e.target.value + "";
-  setMovieData(e.target.selectedIndex, chooseSeat);
-  updateSelectedCount();
-});
 
 // Seat click event
 var getValue = document.getElementById('pickingSeat');
 var pickingSeat = [];
-container.addEventListener('click', (e) => {
-  if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
-    e.target.classList.toggle('selected');
-    updateSelectedCount();
-  }
-  //Convert string to Number
+containerSeat.addEventListener('click', (e) => {
+     //Convert string to Number
   var seatNumber = String(e.target.value);
   //find the seat in the array
   var pos = pickingSeat.indexOf(seatNumber);
+
+ 
 
   //checking
   if (pos >= 0) {
@@ -78,11 +36,15 @@ container.addEventListener('click', (e) => {
   } else {
     pickingSeat.push(seatNumber);
   }
+  if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+    e.target.classList.toggle('selected');
+       
+  }
 
   getValue.value = pickingSeat.toString();
 });
 function changeColor() {
-  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    const selectedSeats = document.querySelectorAll('.seat.selected');
   if (selectedSeats !== null && selectedSeats.length > 0) {
     seats.forEach((seat, index) => {
       if (selectedSeats.indexOf(index) != -1) {
@@ -94,5 +56,5 @@ function changeColor() {
 
 // intial count and total
 changeColor();
-updateSelectedCount();
+
 
